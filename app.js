b42070201afd0aded67b781c7601a003795a93c2
Anam Ahmed <anam.ahmed.a@gmail.com>
2017-11-18 01:40:07 +0600

    forgot to version control, never late to start

---
+++ app.js
@@ -0,0 +1,42 @@
+if(typeof AudioContext!='undefined'){
+  var ctx           = new AudioContext();
+  var osc           = ctx.createOscillator();
+  var gain          = ctx.createGain();
+  var currentFreq   = null;
+  // Initial Setup
+  gain.gain.value=0.5;
+  osc.type="triangle";
+  osc.connect(gain);
+  osc.start(ctx.currentTime);
+  // Event Listeners
+  function keyPress(e){
+    console.log('keypress');
+    var freq = parseFloat(e.target.dataset.frequency);
+    if(!freq){
+      return false;
+    }
+    osc.frequency.value=freq;
+    if(!currentFreq){
+      gain.connect(ctx.destination);
+    }
+    currentFreq=freq;
+  }
+  function keyRelease(e){
+    console.log('release');
+    var freq = parseFloat(e.target.dataset.frequency);
+    if(currentFreq==freq){
+      gain.disconnect(ctx.destination);
+      currentFreq = null;
+    }
+  }
+  var reeds = document.querySelectorAll('.reed');
+  for(i=0;i<reeds.length;i++){
+    reeds[i].addEventListener('mousedown',keyPress);
+    reeds[i].addEventListener('touchstart',keyPress);
+    reeds[i].addEventListener('mouseup',keyRelease);
+    reeds[i].addEventListener('touchend',keyRelease);
+  }
+}
+else{
+  alert("Please use a real browser");
+}
